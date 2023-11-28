ee8de47c35a7485627e9c59537f2f12646bcfbc0
James Starner <jstarner3@gmail.com>
2023-11-27 23:56:22 -0700

    Forgot to push but had a local backup

--- rewear_app/models.py
+++ rewear_app/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 #Created this class
+
+class Poster(models.Model):
+    name = models.CharField(max_length=200)
+    contact_email = models.CharField(max_length=200)
+    about = models.CharField(max_length=200,blank=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, unique=True)
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('poster-detail', args=[str(self.id)]) #I have to change this later
+
 
 class Post(models.Model):
 #List of choices for major value in database, human readable name
@@ -29,7 +45,7 @@ class Post(models.Model):
         ('Other', 'Other')
         )
 
-    image = models.ImageField(upload_to='images')           # Make it into an array later so multiple images can be uploaded
+    image = models.ImageField(upload_to='images', blank=True)           # Make it into an array later so multiple images can be uploaded
     brand = models.CharField(max_length=200)                # might make it a dropdown with an other option
     contact_info = models.CharField(max_length=200)         # Place for user to input info
     type = models.CharField(max_length=200, choices=Type)   # type of clothing
@@ -37,6 +53,8 @@ class Post(models.Model):
     about = models.CharField(max_length=200,blank=True)     # Allows user to input more info
     more_sizing = models.BooleanField(default=False)        # Check allows users to sort by more sizes
     title = models.CharField(max_length=200)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = None)
 
     #Define default String to return the name for representing the Model object."
     def __str__(self):
--- rewear_app/urls.py
+++ rewear_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 from . import views
 
 
@@ -12,10 +12,26 @@ path('', views.index, name='index'),
 #Added in Sprint1
 path('posts/', views.PostListView.as_view(), name= 'posts'),
 path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+path('poster/<int:pk>', views.PosterDetailView.as_view(), name='poster-detail'),
 #Add to path later so that these are linked with a user
-path('create_post/', views.createPost, name='create_post'),
+
+
+
+path('user/<int:user_id>/create_post/', views.createPost, name='create_post'),
 path('update_post/<int:post_id>/', views.updatePost, name='update_post'),
 path('delete_post/<int:post_id>/', views.deletePost, name='delete_post'),
 
+path('accounts/', include('django.contrib.auth.urls')),
+path('accounts/register/', views.registerPage, name= 'register_page'),
+path('user/', views.userPage, name = 'user_page'),
+path('accounts/profile/', views.profilePage, name = 'profile'),
+path('poster/<int:pk>', views.PosterDetailView.as_view(), name='poster-detail'),
 ]
+
+
+"""
+path('portfolio/<int:portfolio_id>/create_project/', views.createProject, name='create_project'),
+path('portfolio/<int:portfolio_id>/update_project/<int:project_id>/', views.updateProject, name='update_project'),
+path('portfolio/<int:portfolio_id>/delete_project/<int:project_id>/', views.deleteProject, name='delete_project'),
+"""
 
